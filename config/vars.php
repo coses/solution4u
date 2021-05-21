763b2a4a7dbc2f3805693642ca267e8838212592
coses <71074247+coses@users.noreply.github.com>
2021-05-21 15:20:47 +0200

    Delete vars.php

--- config/vars.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("SITE_URL", "https://solution4u.herokuapp.com/");
-define("SITE_THEME_URL", SITE_URL."themes/solution4u/");
-define("SITE_THEME_URL_ICON", SITE_URL."themes/solution4u/images/icons/");
-
-$view = $seccion = "home";
