8d889b30eca5b0e9877f6114065bf2b35ba82f3f
coses <71074247+coses@users.noreply.github.com>
2021-05-21 15:01:36 +0200

    Create head.php

---
+++ detectives/themes/solution4u/templates/head.php
@@ -0,0 +1,39 @@
+<title>Solution4U | Agencia de detectives | Roleplay</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Solution4U, agencia de detectives roleplay" />
+    <meta name="keywords" content="Detectives, Agencia, Solution4U, Roleplay, GTA V" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700,900" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>fonts/icomoon/style.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/jquery-ui.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/bootstrap-datepicker.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>fonts/flaticon/font/flaticon.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/aos.css" />
+    <link rel="stylesheet" href="<?php echo SITE_THEME_URL; ?>css/style.css" />
+    <link rel="shortcut icon" href="favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo SITE_THEME_URL_ICON; ?>apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo SITE_THEME_URL_ICON; ?>favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo SITE_THEME_URL_ICON; ?>favicon-16x16.png" />
+    <link rel="manifest" href="<?php echo SITE_THEME_URL_ICON; ?>site.webmanifest" />
+    <link rel="mask-icon" href="<?php echo SITE_THEME_URL_ICON; ?>safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+
+    <meta property="og:title" content="Solution4U, agencia de detectives privados roleplay" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="<?php echo SITE_THEME_URL_ICON; ?>favicon-ogimage.png" />
+    <meta property="og:site_name" content="Solution4U" />
+    <meta property="og:description" content="Solution4U, la agencia de detectives que le ayudará a resolver todos sus problemas, o al menos lo intentará." />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="Solution4U, agencia de detectives privados roleplay" />
+    <meta name="twitter:description" content="Solution4U, la agencia de detectives que le ayudará a resolver todos sus problemas, o al menos lo intentará." />
+    <meta name="twitter:image" content="<?php echo SITE_THEME_URL_ICON; ?>favicon-twittercard.png" />
+    <meta name="twitter:image:src" content="<?php echo SITE_THEME_URL_ICON; ?>favicon-twittercard.png" />
