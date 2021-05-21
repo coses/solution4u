9654a677d6cf5f39867999f0c78fd84cb1833ee7
coses <71074247+coses@users.noreply.github.com>
2021-05-21 14:49:01 +0200

    Update vars.php

--- config/vars.php
+++ config/vars.php
@@ -4,15 +4,3 @@ define("SITE_THEME_URL", SITE_URL."themes/solution4u/");
 define("SITE_THEME_URL_ICON", SITE_URL."themes/solution4u/images/icons/");
 
 $view = $seccion = "home";
-if(isset($_GET["sec"])){
-    $view = $seccion = $_GET["sec"];
-    if(isset($_GET["sub"])){
-        $view = $_GET["sub"];
-    }
-}
-$linkMenu = "";
-$linkActivo = "";
-if($seccion != "home"){
-    $linkMenu = SITE_URL;
-    $linkActivo = " active";
-}
