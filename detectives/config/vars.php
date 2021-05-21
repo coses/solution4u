5bad2b1245e6d0355a40e150bf320a68625ae866
coses <71074247+coses@users.noreply.github.com>
2021-05-21 14:53:46 +0200

    Update vars.php

--- detectives/config/vars.php
+++ detectives/config/vars.php
@@ -1,5 +1,5 @@
 <?php
-define("SITE_URL", "https://solution4u.herokuapp.com/");
+define("SITE_URL", "https://solution4u.herokuapp.com/detectives/");
 define("SITE_THEME_URL", SITE_URL."themes/solution4u/");
 define("SITE_THEME_URL_ICON", SITE_URL."themes/solution4u/images/icons/");
 
