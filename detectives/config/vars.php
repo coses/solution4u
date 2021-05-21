96b59f6cce042c565610274b63269361429cb078
coses <71074247+coses@users.noreply.github.com>
2021-05-21 15:42:27 +0200

    Update vars.php

--- detectives/config/vars.php
+++ detectives/config/vars.php
@@ -2,6 +2,6 @@
 define("SITE_URL", "https://solution4u.herokuapp.com/");
 define("SITE_FOLDER", "detectives/");
 define("SITE_THEME_URL", SITE_URL.SITE_FOLDER."themes/solution4u/");
-define("SITE_THEME_URL_ICON", SITE_URL."themes/solution4u/images/icons/");
+define("SITE_THEME_URL_ICON", SITE_URL.SITE_FOLDER."themes/solution4u/images/icons/");
 
 $view = $seccion = "home";
