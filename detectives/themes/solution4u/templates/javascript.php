855bcd98243eff739fa7d3dcf3ff4b05c2429835
coses <71074247+coses@users.noreply.github.com>
2021-05-21 15:43:56 +0200

    Update javascript.php

--- detectives/themes/solution4u/templates/javascript.php
+++ detectives/themes/solution4u/templates/javascript.php
@@ -1,17 +1,17 @@
-<script src="<?php echo SITE_URL; ?>js/jquery-3.3.1.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery-migrate-3.0.1.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery-ui.js"></script>
-<script src="<?php echo SITE_URL; ?>js/popper.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/bootstrap.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/owl.carousel.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery.stellar.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery.countdown.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/bootstrap-datepicker.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery.easing.1.3.js"></script>
-<script src="<?php echo SITE_URL; ?>js/aos.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery.fancybox.min.js"></script>
-<script src="<?php echo SITE_URL; ?>js/jquery.sticky.js"></script>
-<script src="<?php echo SITE_URL; ?>js/typed.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery-migrate-3.0.1.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery-ui.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/popper.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/bootstrap.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/owl.carousel.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery.stellar.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery.countdown.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery.easing.1.3.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/aos.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery.fancybox.min.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/jquery.sticky.js"></script>
+<script src="<?php echo SITE_URL.SITE_FOLDER; ?>js/typed.js"></script>
 <script>
   var typed = new Typed('.typed-words', {
     strings: ["Cosas"," Otras Cosas"," Cosas Distintas"],
