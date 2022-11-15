7d9165549e6220850bf9851d9c2f202974360ee3
coses <71074247+coses@users.noreply.github.com>
2022-11-15 15:35:56 +0100

    Update vars.php
    Canvi site_url

--- detectives/config/vars.php
+++ detectives/config/vars.php
@@ -1,5 +1,5 @@
 <?php
-define("SITE_URL", "https://solution4u.herokuapp.com/");
+define("SITE_URL", "https://solution4u-production.up.railway.app/");
 define("SITE_FOLDER", "detectives/");
 define("SITE_THEME_URL", SITE_URL.SITE_FOLDER."themes/solution4u/");
 define("SITE_THEME_URL_ICON", SITE_URL.SITE_FOLDER."themes/solution4u/images/icons/");
